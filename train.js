5e2981c950af88b9ac0f74bd887b443ad8b69338
Chad <khasan0203@icloud.com>
2023-05-06 02:03:08 +0900

    fix: Train

---
+++ train.js
@@ -0,0 +1,51 @@
+// console.log("train task ishga tushdi!");
+// A-task
+// function countLetter(harf, soz) {
+//     let count = 0;
+  
+//     for (let i = 0; i < soz.length; i++) {
+//       if (soz[i] === harf) {
+//         count++;
+//       }
+//     }
+  
+//     return count;
+//   }
+//   console.log(countLetter('e', 'engineer'));
+  
+//   console.log("--------------------------------------------");
+
+// B- task
+// function Positive(numbers) {
+//     const positiveNumbers = [];
+  
+//     for (let i = 0; i < numbers.length; i++) {
+//       if (numbers[i] > 0) {
+//         positiveNumbers.push(numbers[i]);
+//       }
+//     }
+  
+//     return positiveNumbers;
+//   }
+  
+//   const numbers = [0, -15, 5, -3, 2, -8, 9, -11];
+//   console.log(Positive(numbers)); 
+
+//   console.log("--------------------------------------------");
+
+// // C-task
+// function checkLetters(word1, word2) {
+  
+//     const harflar1 = word1.split('');
+//     console.log(harflar1);
+//     const harflar2 = word2.split('');
+//     console.log(harflar2);
+//     harflar1.sort();
+//     console.log(harflar1.sort());
+//     harflar2.sort();
+//     console.log(harflar2.sort());
+//     return harflar1.join('') == harflar2.join('');
+//   }
+  
+//   console.log(checkLetters("carselon", "nolesrac")); 
+//   console.log(checkLetters("dog", "game"));
